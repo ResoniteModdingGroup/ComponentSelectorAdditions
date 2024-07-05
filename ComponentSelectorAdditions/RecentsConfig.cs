157b21a9445332639cc9cc21a1b8d03d0e287c80
Arne Kiesewetter <banane9@live.de>
2024-07-05 19:05:48 +0200

    Remove existing instances of recent types; swap to saving types directly

--- ComponentSelectorAdditions/RecentsConfig.cs
+++ ComponentSelectorAdditions/RecentsConfig.cs
@@ -1,4 +1,5 @@
-﻿using MonkeyLoader.Configuration;
+﻿using FrooxEngine;
+using MonkeyLoader.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,8 @@ namespace ComponentSelectorAdditions
     internal sealed class RecentsConfig : ConfigSection
     {
         private static readonly DefiningConfigKey<bool> _addRecentConcreteComponentsToSelection = new("AddRecentConcreteComponentsToSelection", "Ensure that recent concrete versions of generic Components / Nodes appear in the selection.", () => true);
-        private static readonly DefiningConfigKey<List<string>> _componentsKey = new("RecentComponents", "Recent Components", () => new List<string>() { "FrooxEngine.ValueMultiDriver`1", "FrooxEngine.ReferenceMultiDriver`1" }, true);
-        private static readonly DefiningConfigKey<List<string>> _protoFluxNodesKey = new("RecentProtoFluxNodes", "Recent ProtoFlux Nodes", () => new List<string>() { }, true);
+        private static readonly DefiningConfigKey<List<Type>> _componentsKey = new("RecentComponents", "Recent Components", () => new List<Type> { typeof(ValueMultiDriver<>), typeof(ReferenceMultiDriver<>) }, true);
+        private static readonly DefiningConfigKey<List<Type>> _protoFluxNodesKey = new("RecentProtoFluxNodes", "Recent ProtoFlux Nodes", () => new List<Type> { }, true);
 
         private static readonly DefiningConfigKey<int> _recentCapKey = new("TrackedCapacity", "How many recent components / nodes to save and show.", () => 32)
         {
@@ -22,11 +23,11 @@ namespace ComponentSelectorAdditions
         private static readonly DefiningConfigKey<bool> _trackGenericComponentsKey = new("TrackGenericComponents", "Whether the generic version of a recent Component / Node gets saved.", () => true);
 
         public bool AddRecentConcreteComponentsToSelection => _addRecentConcreteComponentsToSelection.GetValue();
-        public List<string> Components => _componentsKey.GetValue()!;
+        public List<Type> Components => _componentsKey.GetValue()!;
         public override string Description => "Contains the recent components.";
         public override string Id => "Recents";
 
-        public List<string> ProtoFluxNodes => _protoFluxNodesKey.GetValue()!;
+        public List<Type> ProtoFluxNodes => _protoFluxNodesKey.GetValue()!;
 
         public int RecentCap => _recentCapKey.GetValue();
         public bool TrackConcreteComponents => _trackConcreteComponentsKey.GetValue();
