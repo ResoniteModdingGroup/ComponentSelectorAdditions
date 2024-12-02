1a90b320d97f920e69e6d4e1b31ffce5ef2be6d1
Arne Kiesewetter <banane9@live.de>
2024-12-02 21:19:55 +0100

    Fix case-(in)sensitivity when picking generic type parameters https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/636

---
+++ ComponentSelectorAdditions/CaseSensitivityFix.cs
@@ -0,0 +1,37 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using MonkeyLoader.Patching;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ComponentSelectorAdditions
+{
+    [HarmonyPatchCategory(nameof(CaseSensitivityFix))]
+    internal sealed class CaseSensitivityFix : Monkey<CaseSensitivityFix>
+    {
+        public override bool CanBeDisabled => true;
+
+        protected override bool OnLoaded()
+        {
+            if (!Enabled)
+                return true;
+
+            GlobalTypeRegistry._nameToSystemType = new(GlobalTypeRegistry._nameToSystemType, StringComparer.OrdinalIgnoreCase);
+            GlobalTypeRegistry._byName = new(GlobalTypeRegistry._byName, StringComparer.OrdinalIgnoreCase);
+
+            return base.OnLoaded();
+        }
+
+        private static void Postfix(AssemblyTypeRegistry __instance)
+        {
+            __instance._typesByFullName = new(__instance._typesByFullName, StringComparer.OrdinalIgnoreCase);
+            __instance._typesByName.dictionary = new(__instance._typesByName.dictionary, StringComparer.OrdinalIgnoreCase);
+            __instance._movedTypes = new(__instance._movedTypes, StringComparer.OrdinalIgnoreCase);
+        }
+
+        private static IEnumerable<MethodBase> TargetMethods()
+            => AccessTools.GetDeclaredConstructors(typeof(AssemblyTypeRegistry), false);
+    }
+}
